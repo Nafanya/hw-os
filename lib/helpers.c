3b8fcb24362f6ebde47de97abc4680bc74c68835
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-07 14:05:27 +0300

    add read_ and write_ implementations

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,9 +1,36 @@
 #include "helpers.h"
 
-ssize_t _read(int fd, void *buf, size_t count) {
-    return 0;
+ssize_t read_(int fd, void *buf, size_t count) {
+    void *cur_pos = buf;
+    ssize_t bytes_read = 0;
+    while (bytes_read < count) {
+        ssize_t rd = read(fd, cur_pos, count - bytes_read);
+        if (rd == -1) {
+            return -1;
+        }
+        if (rd == 0) {
+            return bytes_read;
+        } else {
+            bytes_read += rd;
+            cur_pos += rd;
+        }
+    }
+    return bytes_read;
 }
 
-ssize_t _write(int fd, const void *buf, size_t count) {
-    return 1;
+ssize_t write_(int fd, const void *buf, size_t count) {
+    void *cur_pos = (void *) buf;
+    ssize_t bytes_written = 0;
+    while (bytes_written < count) {
+        ssize_t wr = write(fd, cur_pos, count - bytes_written);
+        if (wr == -1) {
+            return -1;
+        } else if (wr == 0) {
+            return bytes_written;
+        } else {
+            bytes_written += wr;
+            cur_pos += wr;
+        }
+    }
+    return bytes_written;
 }
