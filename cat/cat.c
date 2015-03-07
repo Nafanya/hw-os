69b2fc804c1b3a733630a7779f6785aa280bff49
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-07 16:54:47 +0300

    fixed tabs

--- cat/cat.c
+++ cat/cat.c
@@ -8,15 +8,15 @@ int main() {
     ssize_t bytes_read, bytes_written, off;
     
     while ((bytes_read = read(STDIN_FILENO, buf, BUF_SIZE)) != -1 && bytes_read != 0) {
-		for (off = 0; bytes_read; bytes_read -= bytes_written, off += bytes_written) {
-			if ((bytes_written = write(STDOUT_FILENO, buf + off, (size_t)bytes_read)) == 0) {
+        for (off = 0; bytes_read; bytes_read -= bytes_written, off += bytes_written) {
+            if ((bytes_written = write(STDOUT_FILENO, buf + off, (size_t)bytes_read)) == 0) {
                 return 0;
             } else if (bytes_written == -1) {
                 return 0;
             }
         }
     }
-	if (bytes_read < 0) {
+    if (bytes_read < 0) {
         return 1;
     }
     return 0;
