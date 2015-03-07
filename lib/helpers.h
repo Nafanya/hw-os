d1d550c2a3aef2493faa527c51d4da6fe3bf118d
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-07 14:04:52 +0300

    fixed typos in functions names

--- lib/helpers.h
+++ lib/helpers.h
@@ -3,10 +3,8 @@
 
 #include <unistd.h>
 
-extern
-ssize_t _read(int fd, void *buf, size_t count);
+ssize_t read_(int fd, void *buf, size_t count);
 
-extern
-ssize_t _write(int fd, const void *buf, size_t count);
+ssize_t write_(int fd, const void *buf, size_t count);
 
 #endif // __HELPERS_H_
