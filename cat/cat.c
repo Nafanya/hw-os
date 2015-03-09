69dc3d33efaa7525af91147c9fdc2cf57fb014f7
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-09 17:55:43 +0300

    Update cat.c Now use helpers functions to simplify code.

--- cat/cat.c
+++ cat/cat.c
@@ -4,20 +4,13 @@ const size_t BUF_SIZE = 1024;
 
 int main() {
     char buf[BUF_SIZE];
-    
-    ssize_t bytes_read, bytes_written, off;
-    
-    while ((bytes_read = read(STDIN_FILENO, buf, BUF_SIZE)) != -1 && bytes_read != 0) {
-        for (off = 0; bytes_read; bytes_read -= bytes_written, off += bytes_written) {
-            if ((bytes_written = write(STDOUT_FILENO, buf + off, (size_t)bytes_read)) == 0) {
-                return 0;
-            } else if (bytes_written == -1) {
-                return 0;
-            }
-        }
-    }
-    if (bytes_read < 0) {
-        return 1;
+
+    while (1) {
+	ssize_t bytes_read = read_(STDIN_FILENO, buf, BUF_SIZE);
+	if (bytes_read == 0) {
+	    break;
+	}
+	write_(STDOUT_FILENO, buf, bytes_read);
     }
     return 0;
 }
