4f6562bee9beaf3036d7eb9e8cb9a2ff7db31392
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-09 15:38:17 +0300

    Update helpers.c read_ and write_ now wait in case fd is nonblocking and some data can appear on fd in future.

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,35 +1,35 @@
 #include "helpers.h"
+#include "errno.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
-    void *cur_pos = buf;
     ssize_t bytes_read = 0;
     while (bytes_read < count) {
-        ssize_t rd = read(fd, cur_pos, count - bytes_read);
+        ssize_t rd = read(fd, buf + bytes_read, count - bytes_read);
         if (rd == -1) {
-            return -1;
-        }
-        if (rd == 0) {
+	    if (errno != EAGAIN && errno != EWOULDBLOCK) {
+		break;
+	    }
+        } else if (rd == 0) {
             return bytes_read;
         } else {
             bytes_read += rd;
-            cur_pos += rd;
         }
     }
     return bytes_read;
 }
 
 ssize_t write_(int fd, const void *buf, size_t count) {
-    void *cur_pos = (void *) buf;
     ssize_t bytes_written = 0;
     while (bytes_written < count) {
-        ssize_t wr = write(fd, cur_pos, count - bytes_written);
+        ssize_t wr = write(fd, buf + bytes_written, count - bytes_written);
         if (wr == -1) {
-            return -1;
+	    if (errno != EAGAIN && errno != EWOULDBLOCK) {
+		break;
+	    }
         } else if (wr == 0) {
             return bytes_written;
         } else {
             bytes_written += wr;
-            cur_pos += wr;
         }
     }
     return bytes_written;
