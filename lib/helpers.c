56d0c4e897b3945d01623d64c9eab6a8a20c7aac
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-10 17:30:21 +0300

    Update helpers.c Remove blocking reading. Now if read(write) syscalls return error code -1, pass it further to read_(write_) user.

--- lib/helpers.c
+++ lib/helpers.c
@@ -2,35 +2,35 @@
 #include "errno.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
-    ssize_t bytes_read = 0;
+    size_t bytes_read = 0;
+    ssize_t rd;
     while (bytes_read < count) {
-        ssize_t rd = read(fd, buf + bytes_read, count - bytes_read);
-        if (rd == -1) {
-	    if (errno != EAGAIN && errno != EWOULDBLOCK) {
-		break;
-	    }
-        } else if (rd == 0) {
+	rd = read(fd, (char *)buf + bytes_read, count - bytes_read);
+	if (rd == -1 || rd == 0) {
 	    break;
-        } else {
-            bytes_read += rd;
+	} else {
+	    bytes_read += rd;
         }
+    }
+    if (rd == -1) {
+	return rd;
     }
     return bytes_read;
 }
 
 ssize_t write_(int fd, const void *buf, size_t count) {
-    ssize_t bytes_written = 0;
+    size_t bytes_written = 0;
+    ssize_t wr;
     while (bytes_written < count) {
-        ssize_t wr = write(fd, buf + bytes_written, count - bytes_written);
-        if (wr == -1) {
-	    if (errno != EAGAIN && errno != EWOULDBLOCK) {
-		break;
-	    }
-        } else if (wr == 0) {
+        wr = write(fd, (char *)buf + bytes_written, count - bytes_written);
+        if (wr == -1 || wr == 0) {
 	    break;
-        } else {
-            bytes_written += wr;
+	} else {
+	    bytes_written += wr;
         }
+    }
+    if (wr == -1) {
+	return wr;
     }
     return bytes_written;
 }
