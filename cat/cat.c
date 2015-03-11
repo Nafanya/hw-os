cf95a5c97301217a69875c0b00f4bbc26a661af6
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-11 04:21:54 +0300

    Update cat.c Add proper error handling.

--- cat/cat.c
+++ cat/cat.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include "stdio.h"
 
 const size_t BUF_SIZE = 1024;
 
@@ -9,6 +10,9 @@ int main() {
 	ssize_t bytes_read = read_(STDIN_FILENO, buf, BUF_SIZE);
 	if (bytes_read == 0) {
 	    break;
+	} else if (bytes_read == -1) {
+	    perror("Error: ");
+	    return 1;
 	}
 	write_(STDOUT_FILENO, buf, bytes_read);
     }
