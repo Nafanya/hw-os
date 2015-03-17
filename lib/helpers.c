bc4eb967a8c8597ac66ae7732bf59cd2b3c30d8e
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-18 02:01:15 +0300

    Update helpers.c Add read_until implementation.

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,5 +1,7 @@
-#include "helpers.h"
+#include "stdio.h"
 #include "errno.h"
+
+#include "helpers.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
     size_t bytes_read = 0;
@@ -36,5 +38,20 @@ ssize_t write_(int fd, const void *buf, size_t count) {
 }
 
 ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
-    return 0;
+    size_t bytes_read = 0;
+    while (count > 0) {
+	ssize_t rd = read_(fd, (char *)buf + bytes_read, 1);
+	if (rd == -1) {
+	    return -1;
+	} else if (rd == 0) {
+	    break;
+	}
+	count--;
+	bytes_read++;
+	char c = *((char *) buf + bytes_read - 1);
+	if (c == delimiter) {
+	    break;
+	}
+    }
+    return bytes_read;
 }
