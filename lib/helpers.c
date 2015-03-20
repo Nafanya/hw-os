bad9a889fd3e630f9b551760274a9e16fc1877c7
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-20 15:07:11 +0300

    Update helpers.c

--- lib/helpers.c
+++ lib/helpers.c
@@ -34,18 +34,16 @@ ssize_t write_(int fd, const void *buf, size_t count) {
 ssize_t read_until(int fd, void *buf, size_t count, char delimiter) {
     size_t bytes_read = 0;
     while (1) {
-	ssize_t rd = read(fd, (char *)buf + bytes_read, count - bytes_read);
+	ssize_t rd = read_(fd, (char *)buf + bytes_read, 1);
 	if (rd == -1) {
 	    return -1;
 	} else if (rd == 0) {
 	    return bytes_read;
 	}
-	int i;
-	for (i = 0; i < rd; i++) {
-	    if (*((char *)buf + bytes_read + i) == delimiter) {
-		return bytes_read + rd;
-	    }
+	bytes_read++;
+	char c = *((char *) buf + bytes_read - 1);
+	if (c == delimiter) {
+	    return bytes_read;
 	}
-	bytes_read += rd;
     }
 }
