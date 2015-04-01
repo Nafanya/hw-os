a6aa9ea097c402c4d1820bad471ea901a040c16a
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-04-01 21:59:57 +0300

    Update bufio.h. Add typedef for buf_t struct to simplify code.

--- lib/bufio.h
+++ lib/bufio.h
@@ -3,17 +3,16 @@
 
 #include <unistd.h>
 
-struct buf_t {
+typedef struct buf_t {
 	size_t capacity;
 	size_t size;
 	char * data;
-};
-typedef struct buf_t buf_t;
+} buf_t;
 typedef int fd_t;
 
-struct buf_t *buf_new(size_t capacity);
+buf_t *buf_new(size_t capacity);
 
-void buf_free(struct buf_t *);
+void buf_free(buf_t *);
 
 size_t buf_capacity(buf_t *);
 
