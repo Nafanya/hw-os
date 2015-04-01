c1f792c651aae0b914b302dc86ccde76d019b04b
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-04-01 21:25:51 +0300

    Update bufio.c Add implementation for simple (to implement) functions.

--- lib/bufio.c
+++ lib/bufio.c
@@ -2,22 +2,34 @@
 
 struct buf_t *buf_new(size_t capacity) {
 	buf_t buf = (buf_t *) malloc(sizeof(buf_t));
+	if (buf == NULL) {
+		return NULL;
+	}
+	buf->capacity = capacity;
+	buf->size = 0;
+	buf->data = (char *) malloc(capacity * sizeof(char));
+	if (buf->data == NULL) {
+		free(buf);
+		return NULL;
+	}
+	return buf;
 }
 
-void buf_free(struct buf_t *) {
-
+void buf_free(buf_t * buf) {
+	free(buf->data);
+	free(buf);
 }
 
-size_t buf_capacity(buf_t *) {
-
+size_t buf_capacity(buf_t * buf) {
+	return buf->capacity;
 }
 
-size_t buf_size(buf_t *) {
-
+size_t buf_size(buf_t * buf) {
+	return buf->size;
 }
 
 ssize_t buf_fill(fd_t fd, buf_t *buf, size_t required) {
-
+	
 }
 
 ssize_t buf_flush(fd_t fd, buf_t *buf, size_t required) {
