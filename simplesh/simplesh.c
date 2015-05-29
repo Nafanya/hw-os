a8228d4f71215a7c27615a3614b41ce12b4f69ca
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-05-29 05:50:45 +0300

    Add simplesh.c

--- simplesh/simplesh.c
+++ simplesh/simplesh.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "helpers.h"
+
+const int MAX_SIZE = 8192;
 
 void test() {
   char* argv1[] = {"find", "/", NULL};
@@ -21,6 +24,37 @@ void test() {
 }
 
 int main(int argc, char *argv[]) {
-  test();
+  char buf[MAX_SIZE];
+  size_t has = 0;
+  ssize_t rd;
+  while (1) {
+    if (rd = write_(STDOUT_FILENO, "$", 1) == -1) {
+      return EXIT_FAILURE;
+    }
+    if ((rd = read_(STDIN_FILENO, buf, buf + has, MAX_SIZE - has)) == -1) {
+      return EXIT_FAILURE;
+    }
+    int pipes = 1;
+    int lf = -1;
+    for (int i = 0; i < has + rd; i++) {
+      if (buf[i] == '|') {
+        pipes++;
+      } else if (buf[i] == '\n') {
+        lf = i;
+        buf[i] = '\0';
+        break;
+      }
+    }
+    execargs_t** progs = malloc(sizeof(execargs_t) * pipes);
+    char* tok_prog = strtok(buf, "|");
+    while (tok_prog != NULL) {
+      int argcnt = 0;
+      char** args = malloc(sizeof(char*));
+      char* tok_arg = strtok(tok_prog, " ");
+      while (tok_arg != NULL) {
+        
+      }
+    }
+  }
   return 0;
 }
