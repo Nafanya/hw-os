7a74995f43b26b2215e9774b8f409d8c98a371ed
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-05-29 05:20:28 +0300

    Update helpers.c

--- lib/helpers.c
+++ lib/helpers.c
@@ -3,9 +3,18 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #include "helpers.h"
+
+#ifdef DEBUG
+#define DBG(msg) printf("%s AT %s:%d\n", msg, __FILE__, __LINE__);
+#else
+#define DBG(msg)
+#endif
 
 ssize_t read_(int fd, void *buf, size_t count) {
     size_t bytes_read = 0;
@@ -83,9 +92,73 @@ int spawn(const char * file, char * const argv[]) {
 }
 
 int exec(execargs_t* args) {
-  return 0;
+  pid_t pid = fork();
+  if (pid == -1) {
+    return -1;
+  } else if (pid == 0) {
+    /*
+      set child's group same as parent's
+      to send SIGINT to all children catching SIGINT in parent
+    */
+    signal(SIGINT, SIG_DFL);
+    execvp(args->prog, args->args);
+    exit(EXIT_FAILURE);
+  }
+  return pid;
 }
 
+static struct sigaction prev;
+static int this_stdin;
+static int this_stdout;
+static int* pids;
+static int pids_cnt;
+
+void stopall() {
+  for (int i = 0; i < pids_cnt; i++) {
+    kill(pids[i], SIGTERM);
+    waitpid(pids[i], NULL, 0);
+  }
+}
+
+void sig_handler(int sig) {
+  stopall();
+}
+
+#define CHK(_res)                               \
+  if (_res == -1) {                             \
+    stopall();                                  \
+    return -1;                                  \
+  }
+
 int runpiped(execargs_t** programs, size_t n) {
+  struct sigaction sa;
+  sa.sa_handler = sig_handler;
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+  CHK(sigaction(SIGINT, &sa, &prev));
+
+  int pipes[2 * n];
+  int children_pids[n];
+  pids = children_pids;
+  pids_cnt = n;
+  this_stdin = dup(STDIN_FILENO);
+  this_stdout = dup(STDOUT_FILENO);
+
+  for (int i = 0; i < n - 1; i++) {
+    CHK(pipe(pipes + 2 * i));
+    int stdin = pipes[i * 2];
+    int stdout = pipes[i * 2 + 1];
+    // can't make it work with dup3, so call fcntl instead
+    CHK(fcntl(stdout, F_SETFD, FD_CLOEXEC));
+    CHK(dup2(stdout, STDOUT_FILENO));
+    children_pids[i] = exec(programs[i]);
+    CHK(children_pids[i]);
+    CHK(dup2(stdin, STDIN_FILENO));
+  }
+  CHK(dup2(this_stdout, STDOUT_FILENO));
+  CHK(close(this_stdout));
+  children_pids[n - 1] = exec(programs[n - 1]);
+  CHK(children_pids[n - 1]);
+  wait(NULL);
   return 0;
 }
